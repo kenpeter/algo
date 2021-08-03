8a8ec318cc3679e433f624eca289215db8b7e4d2
Gary Liang <gary.liang@nab.com.au>
2021-08-03 21:38:49 +1000

    WIP: update

--- company/amazon/fresh_promition_apple_apple/index.js
+++ company/amazon/fresh_promition_apple_apple/index.js
@@ -1,45 +1,120 @@
 // https://leetcode.com/playground/gkNkp5nV
 // https://leetcode.com/discuss/interview-question/1002811/Amazon-or-OA-2021-or-Fresh-Promotion
 
-const freshPromotion = (codesIn, cart) => {
-  const code = [];
+// function freshPromotion(codeList, shoppingCart) {
+//   // code list
+//   let listLen = codeList.length;
+//   // card list
+//   let cartLen = shoppingCart.length;
+//   let result = [];
 
-  // * group order important
-  // * item in 1 group important
-  // * so 2d -> 1d
-  for (let i = 0; i < codesIn.length; ++i) {
-    for (let j = 0; j < codesIn[i].length; ++j) {
-      code.push(codesIn[i][j]);
-    }
-  }
+//   let checkCurrentGroup = (listCode, cart, i, j) => {
+//     if (
+//       (j == cart.length - 1 || i == listCode.length - 1) &&
+//       (listCode[i] == cart[j] || listCode[i + 1] == "anything")
+//     )
+//       return 1;
 
-  // no code, always win
-  if (code.length === 0) return 1;
+//     if (
+//       listCode[i + 1] &&
+//       cart[j + 1] &&
+//       (listCode[i + 1] == cart[j + 1] || listCode[i + 1] == "anything") &&
+//       checkCurrentGroup(listCode, cart, i + 1, j + 1)
+//     )
+//       return 1;
 
-  // too much code
-  if (code.length > cart.length) return 0;
+//     return 0;
+//   };
 
-  // * 2pt
-  let i = 0;
+//   for (let i = 0; i < listLen; i++) {
+//     let currentListLen = codeList[i].length;
+//     cartLen = shoppingCart.length;
+//     if (currentListLen <= cartLen) {
+//       for (let j = 0; j < cartLen; j++) {
+//         if (
+//           cartLen - j >= currentListLen &&
+//           (codeList[i][0] == shoppingCart[j] || codeList[i][0] == "anything")
+//         ) {
+//           let codeResult = checkCurrentGroup(codeList[i], shoppingCart, 0, j);
+//           result.push(codeResult);
+//           if (codeResult == 1) shoppingCart.splice(j, j + currentListLen);
+//           break;
+//         }
+//       }
+//     }
+//   }
+
+//   if (result.length < codeList.length) {
+//     return 0;
+//   } else {
+//     return result.includes(0) ? 0 : 1;
+//   }
+// }
+
+// const freshPromotion = (codesIn, cart) => {
+//   const code = [];
+
+//   // * ele order, group order; 2D -> 1D
+//   for (let i = 0; i < codesIn.length; ++i) {
+//     for (let j = 0; j < codesIn[i].length; ++j) {
+//       code.push(codesIn[i][j]);
+//     }
+//   }
+
+//   // no code, always win
+//   if (code.length === 0) return 1;
+
+//   // too much code
+//   if (code.length > cart.length) return 0;
+
+//   // * 2pt
+//   let i = 0;
+//   let j = 0;
+//   while (i < code.length && j < cart.length) {
+//     // * vertical compare; anything, both_move
+//     if (code[i] === cart[j] || code[i] === "anything") {
+//       ++i;
+//       ++j;
+//     } else {
+//       ++j; // keep going
+//     }
+//   } // end loop
+
+//   // code done
+//   if (i === code.length) {
+//     return 1;
+//   } else {
+//     return 0;
+//   }
+// };
+
+// sm
+// * chars in substr (order); substrs in sentense (order, gap)
+// * vertical_match or !match, reset substr
+const freshPromotion = (codes, cart) => {
   let j = 0;
-  while (i < code.length && j < cart.length) {
-    // * vertical compare; anything, both_move
-    // * continue or reset
-    if (code[i] === cart[j] || code[i] === "anything") {
-      ++i;
-      ++j;
-    } else {
-      i = 0;
-      ++j;
-    }
-  } // end loop
+  // substrs
+  for (const code of codes) {
+    // single substr
+    let i = 0;
+    while (i < code.length && j < cart.length) {
+      // vertical match
+      if (code[i] === cart[j] || code[i] === "anything") {
+        ++i;
+        ++j;
+      } else {
+        // !match, reset substr
+        i = 0;
+        // skip the gap
+        ++j;
+      }
+    } // end loop
 
-  // code done
-  if (i === code.length) {
-    return 1;
-  } else {
-    return 0;
+    // substr
+    if (i !== code.length) return 0;
   }
+
+  return 1;
 };
 
 // const codeList = [
@@ -47,7 +122,6 @@ const freshPromotion = (codesIn, cart) => {
 //   ["banana", "anything", "banana"],
 // ];
 // const shoppingCart = ["orange", "apple", "apple", "banana", "orange", "banana"];
-
 // const out = freshPromotion(codeList, shoppingCart);
 // console.log(out);
 
@@ -68,10 +142,27 @@ const freshPromotion = (codesIn, cart) => {
 // const out = freshPromotion(codeList, shoppingCart);
 // console.log(out);
 
+// const codeList = [
+//   ["apple", "apple"],
+//   ["apple", "apple", "banana"],
+// ];
+// const shoppingCart = ["apple", "apple", "apple", "banana"];
+// const out = freshPromotion(codeList, shoppingCart);
+// console.log(out);
+
 const codeList = [
   ["apple", "apple"],
-  ["apple", "apple", "banana"],
+  ["banana", "anything", "banana"],
 ];
-const shoppingCart = ["apple", "apple", "apple", "banana"];
+const shoppingCart = [
+  "apple",
+  "apple",
+  "orange",
+  "orange",
+  "banana",
+  "apple",
+  "banana",
+  "banana",
+];
 const out = freshPromotion(codeList, shoppingCart);
 console.log(out);
--- company/amazon/fresh_promition_apple_apple/readme.md
+++ company/amazon/fresh_promition_apple_apple/readme.md
@@ -1,5 +1,3 @@
-https://leetcode.com/discuss/interview-question/1002811/Amazon-or-OA-2021-or-Fresh-Promotion
-
 Amazon is running a promotion in which customers receive prizes for purchasing a secret combination of fruits. The combination will change each day, and the team running the promotion wants to use a code list to make it easy to change the combination. The code list contains groups of fruits. Both the order of the groups within the code list and the order of the fruits within the groups matter. However, between the groups of fruits, any number, and type of fruit is allowable. The term "anything" is used to allow for any type of fruit to appear in that location within the group.
 
 Consider the following secret code list: [[apple, apple], [banana, anything, banana]]
