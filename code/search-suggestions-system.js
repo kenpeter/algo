71720ccfe422b9e7b71cb74f8168fbf939ae2e4f
Gary Liang <gary.liang@nab.com.au>
2021-08-09 14:39:46 +1000

    WIP: update

--- code/search-suggestions-system.js
+++ code/search-suggestions-system.js
@@ -3,6 +3,7 @@
 // // * each_char == level_of_filter
 // // * (1) no update pool (2) update pool (once outloop)*; (3) update pool (inner loop);
 // // * 2D_arr[each_char][result]
+// // * e.g. ["abc", "def"] -> 2D_arr
 // var suggestedProducts = function (ps, s) {
 //   const res = [];
 //   ps.sort();
@@ -31,29 +32,72 @@
 //   return res;
 // };
 
-// sm:
-// * brute_force
-// * (1) no update pool* (2) update pool (once outloop); (3) update pool (inner loop);
-// * w.lastIndexOf(substr, 0) === 0
-var suggestedProducts = function (ps, s) {
-  const res = [];
-  ps.sort();
+// // sm:
+// // * brute_force
+// // * w.lastIndexOf(substr, 0) === 0
+// // * (1) no update pool* (2) update pool (once outloop); (3) update pool (inner loop);
+// // * 2D_arr[each_char][result]
+// // * e.g. ["abc", "def"] -> 2D_arr
+// var suggestedProducts = function (ps, s) {
+//   const res = [];
+//   ps.sort();
 
-  for (let i = 0; i < s.length; ++i) {
-    const str = s.substring(0, i + 1);
-    res.push([]);
+//   for (let i = 0; i < s.length; ++i) {
+//     const str = s.substring(0, i + 1);
+//     res.push([]);
 
-    // no update pool
-    for (let j = 0; j < ps.length; ++j) {
-      const w = ps[j];
+//     // no update pool
+//     for (let j = 0; j < ps.length; ++j) {
+//       const w = ps[j];
 
-      if (res[i].length <= 2 && w.lastIndexOf(str, 0) === 0) {
-        res[i].push(w);
-      }
+//       if (res[i].length <= 2 && w.lastIndexOf(str, 0) === 0) {
+//         res[i].push(w);
+//       }
+//     }
+//   }
+
+//   return res;
+// };
+
+const suggestedProducts = (products, searchWord) => {
+  // 2D arr
+  const result = Array.from({ length: searchWord.length }, () => []);
+  // left
+  let left = 0;
+  // right
+  let right = products.length - 1;
+  // sort
+  products.sort();
+
+  // loop char in search_word
+  for (let idx = 0; idx < searchWord.length; ++idx) {
+    // target char
+    const targetChar = searchWord[idx];
+
+    //
+    for (let low = left, high = right; low < high; ) {
+      // mid
+      const mid = Math.floor((high + low) / 2);
+
+      products[mid][idx] === undefined || products[mid][idx] < targetChar
+        ? (low = mid + 1)
+        : (high = mid);
+
+      //
+      left = low;
     }
+    for (let low = left, high = right; low < high; ) {
+      const mid = Math.ceil((high + low) / 2);
+      products[mid][idx] > targetChar ? (high = mid - 1) : (low = mid);
+      right = high;
+    }
+    for (let i = 0; i < 3; ++i) {
+      if (left + i > right || products[left + i][idx] !== targetChar) break;
+      result[idx].push(products[left + i]);
+    }
+    if (result[idx].length === 0) break;
   }
-
-  return res;
+  return result;
 };
 
 // sort: mobile, moneypot, monitor, mouse, mousepad
