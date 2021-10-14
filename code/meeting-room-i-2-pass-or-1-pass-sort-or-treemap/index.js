4762514c54c8ec4bb36187031820e236a9a3084f
Gary Liang <gary.liang@nab.com.au>
2021-10-14 17:05:32 +1100

    WIP: up

--- code/meeting-room-i-2-pass-or-1-pass-sort-or-treemap/index.js
+++ code/meeting-room-i-2-pass-or-1-pass-sort-or-treemap/index.js
@@ -35,22 +35,57 @@
 // const out = meetingRoom(a);
 // console.log(out);
 
-// method 2: 1 pass, sort, corss edge check
+// // method 2: 1 pass, sort, corss edge check
+// const meetingRoom = (a) => {
+//   // sort
+//   a.sort((a, b) => {
+//     return a[0] - b[1];
+//   });
+
+//   // 1 pass
+//   for (let i = 1; i < a.length; ++i) {
+//     const prev = a[i - 1];
+//     const curr = a[i];
+
+//     // cross edge check
+//     if (curr[0] < prev[1] && curr[1] > prev[0]) {
+//       return false;
+//     }
+//   } // el
+
+//   return true;
+// };
+
+// const a = [
+//   [0, 30],
+//   [5, 10],
+//   [15, 20],
+// ];
+// const out = meetingRoom(a);
+// console.log(out);
+
+// method 3: treemap
 const meetingRoom = (a) => {
-  // sort
-  a.sort((a, b) => {
-    return a[0] - b[1];
-  });
+  const h = {};
 
-  // 1 pass
-  for (let i = 1; i < a.length; ++i) {
-    const prev = a[i - 1];
-    const curr = a[i];
+  for (let i = 0; i < a.length; ++i) {
+    const i_s = a[i][0];
+    const i_e = a[i][1];
 
-    // cross edge check
-    if (curr[0] < prev[1] && curr[1] > prev[0]) {
-      return false;
-    }
+    // assume
+    h[i_s] = (h[i_s] || 0) + 1;
+    h[i_e] = (h[i_e] || 0) - 1;
+
+    // check
+    let c = 0;
+    for (let k in h) {
+      c = c + h[k];
+      if (c > 1) {
+        return false;
+      }
+    } // el
+
+    // no need to revert, as assume it is fact
   } // el
 
   return true;
