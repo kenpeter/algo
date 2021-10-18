b06fbd0486758a78f7229edee07758d38ab8b5a4
Gary Liang <gary.liang@nab.com.au>
2021-10-18 11:39:36 +1100

    WIP: up

--- code/employee-free-time-no-care-individual-start-vs-max-end-time/index.js
+++ code/employee-free-time-no-care-individual-start-vs-max-end-time/index.js
@@ -1,3 +1,40 @@
+// // sm:
+// // * no care individual
+// // * vitual merge (max_end_time)
+// const freeTime = (arr) => {
+//   const a = [];
+//   // no care individual
+//   for (let i = 0; i < arr.length; ++i) {
+//     const inter = arr[i];
+//     for (let j = 0; j < inter.length; ++j) {
+//       const s = inter[j][0];
+//       const e = inter[j][1];
+//       a.push([s, e]);
+//     }
+//   }
+
+//   a.sort((a, b) => a[0] - b[0]);
+
+//   const res = [];
+//   let max = a[0][1];
+//   // virtual merge (max_end_time)
+//   for (let i = 1; i < a.length; ++i) {
+//     const s = a[i][0];
+//     const e = a[i][1];
+
+//     if (s > max) {
+//       res.push([max, s]);
+//     }
+
+//     max = Math.max(max, e);
+//   }
+
+//   return res;
+// };
+
+// sm:
+// * no care individual
+// * actual merge
 const freeTime = (arr) => {
   const a = [];
   // no care individual
@@ -13,18 +50,30 @@ const freeTime = (arr) => {
   a.sort((a, b) => a[0] - b[0]);
 
   const res = [];
-  let max = a[0][1];
-  // start vs max_end_time
+  const merge = [a[0]];
+
+  // actual merge
   for (let i = 1; i < a.length; ++i) {
-    const s = a[i][0];
-    const e = a[i][1];
+    // income
+    const i_s = a[i][0];
+    const i_e = a[i][1];
 
-    if (s > max) {
-      res.push([max, s]);
+    // in queue
+    const ele = merge.pop();
+    const s = ele[0];
+    const e = ele[1];
+
+    // inter
+    if (e >= i_s) {
+      merge.push([Math.min(i_s, s), Math.max(i_e, e)]);
+    } else {
+      merge.push(ele);
+      merge.push([i_s, i_e]);
+
+      // gap
+      res.push([e, i_s]);
     }
-
-    max = Math.max(max, e);
-  }
+  } // el
 
   return res;
 };
