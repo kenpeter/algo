054262e8d96a84b59dabe0c1c00bd565be16b705
Gary Liang <gary.liang@nab.com.au>
2022-01-04 12:57:15 +1100

    WIP: up

---
+++ code/132-pattern-left_smallest-mid_largest-right_2nd_largest--search_in_stack_desc/index.js
@@ -0,0 +1,30 @@
+var find132pattern = function (ns) {
+  const s = [];
+  let sec = Number.MIN_SAFE_INTEGER;
+
+  for (let i = ns.length - 1; i >= 0; --i) {
+    // 12 pattern
+    if (ns[i] < sec) {
+      return true;
+    }
+
+    // * 32 pattern
+    // * stack desc, end is largest, small near you (2nd largest)
+    while (s.length > 0 && ns[i] > s[s.length - 1]) {
+      sec = s.pop();
+    } // el
+
+    // * stack desc, end is largest, small near you.
+    s.push(ns[i]);
+  } // el
+
+  return false;
+};
+
+// const ns = [3, 5, 0, 3, 4];
+// const out = find132pattern(ns);
+// console.log(out);
+
+const ns = [3, 1, 4, 2];
+const out = find132pattern(ns);
+console.log(out);
