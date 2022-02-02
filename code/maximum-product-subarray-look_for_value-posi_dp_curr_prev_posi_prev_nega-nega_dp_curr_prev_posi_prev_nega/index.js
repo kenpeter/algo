4f17f7af13610503503ea782b4f3c9591b76713f
Gary Liang <gary.liang@nab.com.au>
2022-02-02 23:44:34 +1100

    WIP: up

--- code/maximum-product-subarray-look_for_value-posi_dp_curr_prev_posi_prev_nega-nega_dp_curr_prev_posi_prev_nega/index.js
+++ code/maximum-product-subarray-look_for_value-posi_dp_curr_prev_posi_prev_nega-nega_dp_curr_prev_posi_prev_nega/index.js
@@ -1,20 +1,18 @@
-// sm: posi_dp, nega_dp, prev_state * curr
 var maxProduct = function (ns) {
-  const p = [];
-  const n = [];
-  p[0] = ns[0];
-  n[0] = ns[0];
+  const pdp = Array(ns.length).fill(0);
+  const ndp = Array(ns.length).fill(0);
 
-  for (let i = 1; i < ns.length; i++) {
+  pdp[0] = ns[0];
+  ndp[0] = ns[0];
+
+  for (let i = 1; i < ns.length; ++i) {
     const curr = ns[i];
-    p[i] = Math.max(curr, p[i - 1] * curr, n[i - 1] * curr);
-    n[i] = Math.min(curr, p[i - 1] * curr, n[i - 1] * curr);
-  }
 
-  //console.log('+++++ p', p);
-  //console.log('----- n', n);
+    pdp[i] = Math.max(curr, pdp[i - 1], pdp[i - 1] * curr, ndp[i - 1] * curr);
+    ndp[i] = Math.min(curr, ndp[i - 1], pdp[i - 1] * curr, ndp[i - 1] * curr);
+  } // el
 
-  return Math.max(...p);
+  return Math.max(...pdp);
 };
 
 const ns = [2, 3, -2, 4];
